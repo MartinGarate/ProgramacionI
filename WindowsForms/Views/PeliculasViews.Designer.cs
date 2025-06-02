45f50ec2b812c08ffac5aa1a3db5f3689cce616e
MartinGarate <martingarate100@gmail.com>
2025-06-02 08:33:11 -0300

    Implementa vista de películas y clase Pelicula
    Se ha modificado la clase principal para iniciar `PeliculasViews` en lugar de `FormatoTextoView`.
    Se ha añadido la clase `Pelicula` para representar los detalles de una película.
    La clase `PeliculasViews` ahora realiza una llamada HTTP asíncrona para obtener y mostrar películas en un `DataGridView`.
    Además, se ha diseñado la interfaz de usuario y se ha creado un archivo de recursos para la aplicación.

---
+++ WindowsForms/Views/PeliculasViews.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace WindowsForms.Views
+{
+    partial class PeliculasViews
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            btnAgregar = new FontAwesome.Sharp.IconButton();
+            btnBorrar = new FontAwesome.Sharp.IconButton();
+            btnEditar = new FontAwesome.Sharp.IconButton();
+            btnSalir = new FontAwesome.Sharp.IconButton();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 12);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.Size = new Size(787, 234);
+            dataGridView1.TabIndex = 0;
+            // 
+            // btnAgregar
+            // 
+            btnAgregar.IconChar = FontAwesome.Sharp.IconChar.None;
+            btnAgregar.IconColor = Color.Black;
+            btnAgregar.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            btnAgregar.Location = new Point(301, 283);
+            btnAgregar.Name = "btnAgregar";
+            btnAgregar.Size = new Size(75, 23);
+            btnAgregar.TabIndex = 1;
+            btnAgregar.Text = "&Agregar";
+            btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // btnBorrar
+            // 
+            btnBorrar.IconChar = FontAwesome.Sharp.IconChar.None;
+            btnBorrar.IconColor = Color.Black;
+            btnBorrar.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            btnBorrar.Location = new Point(463, 283);
+            btnBorrar.Name = "btnBorrar";
+            btnBorrar.Size = new Size(75, 23);
+            btnBorrar.TabIndex = 2;
+            btnBorrar.Text = "&Borrar";
+            btnBorrar.UseVisualStyleBackColor = true;
+            // 
+            // btnEditar
+            // 
+            btnEditar.IconChar = FontAwesome.Sharp.IconChar.None;
+            btnEditar.IconColor = Color.Black;
+            btnEditar.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            btnEditar.Location = new Point(382, 283);
+            btnEditar.Name = "btnEditar";
+            btnEditar.Size = new Size(75, 23);
+            btnEditar.TabIndex = 3;
+            btnEditar.Text = "&Editar";
+            btnEditar.UseVisualStyleBackColor = true;
+            // 
+            // btnSalir
+            // 
+            btnSalir.IconChar = FontAwesome.Sharp.IconChar.None;
+            btnSalir.IconColor = Color.Black;
+            btnSalir.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            btnSalir.Location = new Point(724, 324);
+            btnSalir.Name = "btnSalir";
+            btnSalir.Size = new Size(75, 23);
+            btnSalir.TabIndex = 4;
+            btnSalir.Text = "&Salir";
+            btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // PeliculasViews
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(811, 359);
+            Controls.Add(btnSalir);
+            Controls.Add(btnEditar);
+            Controls.Add(btnBorrar);
+            Controls.Add(btnAgregar);
+            Controls.Add(dataGridView1);
+            Name = "PeliculasViews";
+            Text = "PeliculasViews";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private FontAwesome.Sharp.IconButton btnAgregar;
+        private FontAwesome.Sharp.IconButton btnBorrar;
+        private FontAwesome.Sharp.IconButton btnEditar;
+        private FontAwesome.Sharp.IconButton btnSalir;
+    }
+}
---
+++ WindowsForms/Views/PeliculasViews.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsForms.Models;
+
+namespace WindowsForms.Views
+{
+    public partial class PeliculasViews : Form
+    {
+        public PeliculasViews()
+        {
+            InitializeComponent();
+            ObtenemosPeliculas(); 
+        }
+
+        private async void ObtenemosPeliculas()
+        {
+            string url = "https://netflisp-77fe.restdb.io/rest/peliculas?apikey=7e3b2f3b47495345da5617567d822a55dfd7f";
+
+            var clientHttp = new HttpClient();
+            var response = await clientHttp.GetAsync(url);
+            if (response != null)
+            {
+                List <Pelicula> peliculas = await response.Content.ReadFromJsonAsync<List<Pelicula>>();
+                dataGridView1.DataSource = peliculas;
+
+            }
+        }
+    }
+}
